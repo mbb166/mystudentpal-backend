ad0b42c9f15bd6b96af5e05b8bd8b2f0d4f7d075
mbb166 <mb.boral@gmail.com>
2017-12-04 12:59:43 +0100

    move class run

--- src/main/java/pl/kdreamteams/MyStudentPal.java
+++ src/main/java/pl/kdreamteams/MyStudentPal.java
@@ -1,4 +1,4 @@
-package pl.kdreamteams.configuration;
+package pl.kdreamteams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
