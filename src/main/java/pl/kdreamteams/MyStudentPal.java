a82b71b9d1514a634217551fed8a9b4758ed148f
mbb166 <mb.boral@gmail.com>
2017-12-04 11:25:08 +0100

    init

---
+++ src/main/java/pl/kdreamteams/MyStudentPal.java
@@ -0,0 +1,11 @@
+package pl.kdreamteams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudentPal {
+    public static void main(String... args) {
+        SpringApplication.run(MyStudentPal.class, args);
+    }
+}
---
+++ src/main/java/pl/kdreamteams/controller/TestController.java
@@ -0,0 +1,15 @@
+package pl.kdreamteams.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "das";
+    }
+}
