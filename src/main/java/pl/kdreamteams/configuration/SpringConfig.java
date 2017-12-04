8e812263d4f6a6193978e169e91fdb99ff765a8c
daniel.babiak <daniel.babiak8@gmail.com>
2017-12-04 22:22:05 +0100

    CORS configurations

--- src/main/java/pl/kdreamteams/configuration/SpringConfig.java
+++ src/main/java/pl/kdreamteams/configuration/SpringConfig.java
@@ -3,6 +3,9 @@ package pl.kdreamteams.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 //@EnableWebMvc
 @Configuration
@@ -17,5 +20,15 @@ public class SpringConfig {
         driverManagerDataSource.setUsername("postgres");
         driverManagerDataSource.setPassword("postgre");
         return driverManagerDataSource;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
