db8c6b2dd37f3e65d3996ee769624fa28a73d6d2
wbedkowski <woj.bedkowski@gmail.com>
2017-12-04 17:47:58 +0100

    Jest ok.

--- src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
+++ src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
@@ -4,9 +4,9 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import pl.kdreamteams.model.*;
+
 
 @Configuration
-public class MainConfiguration extends WebMvcConfigurerAdapter{
+public class MainConfiguration{
 
 }
