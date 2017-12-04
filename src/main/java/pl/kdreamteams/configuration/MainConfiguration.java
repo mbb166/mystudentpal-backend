941ed7db3969a2cd90dd31a80dfc7ca84c95074c
mbb166 <mb.boral@gmail.com>
2017-12-04 15:05:04 +0100

    add json integration

--- src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
+++ src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
@@ -4,8 +4,13 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 public class MainConfiguration extends WebMvcConfigurerAdapter{
@@ -14,6 +19,19 @@ public class MainConfiguration extends WebMvcConfigurerAdapter{
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         super.configureContentNegotiation(configurer);
         configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    }
+
+    @Bean
+    public ViewResolver cnViewResolver(ContentNegotiationManager cnm) {
+        ContentNegotiatingViewResolver cnvr =
+                new ContentNegotiatingViewResolver();
+        cnvr.setContentNegotiationManager(cnm);
+        return cnvr;
+    }
+
+    @Bean
+    public View globalView() {
+        return new MappingJackson2JsonView();
     }
 
     @Bean
