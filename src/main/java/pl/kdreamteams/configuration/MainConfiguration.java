eea60f71993c120b6997a29a4ab91c726c22bb89
mbb166 <mb.boral@gmail.com>
2017-12-04 12:53:17 +0100

    fix

---
+++ src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
@@ -0,0 +1,16 @@
+package pl.kdreamteams.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MainConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        super.configureContentNegotiation(configurer);
+        configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    }
+}
