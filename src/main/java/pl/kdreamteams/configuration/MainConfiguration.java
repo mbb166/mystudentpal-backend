b11413fcaec177d30a404c586c72a078daa122c8
mbb166 <mb.boral@gmail.com>
2017-12-04 20:06:29 +0100

    Delete test controller

--- src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
+++ src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
@@ -1,10 +1,6 @@
 package pl.kdreamteams.configuration;
 
-import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 
 @Configuration
 public class MainConfiguration{
