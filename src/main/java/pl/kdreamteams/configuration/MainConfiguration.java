17e89909be9f6ee2388a8ff6c4063d5e02b89bbc
mbb166 <mb.boral@gmail.com>
2017-12-04 13:25:20 +0100

    Hibernate configuration

--- src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
+++ src/main/java/pl/kdreamteams/configuration/MainConfiguration.java
@@ -1,5 +1,7 @@
 package pl.kdreamteams.configuration;
 
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
@@ -12,5 +14,10 @@ public class MainConfiguration extends WebMvcConfigurerAdapter{
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         super.configureContentNegotiation(configurer);
         configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    }
+
+    @Bean
+    public SessionFactory getSessionFactory() {
+        return new org.hibernate.cfg.Configuration().buildSessionFactory();
     }
 }
--- src/main/java/pl/kdreamteams/configuration/SpringConfig.java
+++ src/main/java/pl/kdreamteams/configuration/SpringConfig.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+
 
 @EnableWebMvc
 @Configuration
@@ -23,14 +22,5 @@ public class SpringConfig extends WebSecurityConfigurerAdapter{
         driverManagerDataSource.setUsername("root");
         driverManagerDataSource.setPassword("password");
         return driverManagerDataSource;
-    }
-
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/pages/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
     }
 }
